72ca614912509830aad2c11cdfeced1e7ca1ce74
wn1980 <wn1980@hotmail.com>
2022-01-25 14:44:47 +0700

    add description

---
+++ launch/description.launch.py
@@ -0,0 +1,63 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, EnvironmentVariable
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    urdf_path = PathJoinSubstitution(
+        [FindPackageShare("turtlebot2_ros2"), "urdf", "kobuki_standalone.urdf.xacro"]
+    )
+
+    rviz_config_path = PathJoinSubstitution(
+        [FindPackageShare('turtlebot2_ros2'), 'rviz', 'robot.rviz']
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            name='urdf', 
+            default_value=urdf_path,
+            description='URDF path'
+        ),
+        
+        DeclareLaunchArgument(
+            name='use_sim_time', 
+            default_value='false',
+            description='Use simulation time'
+        ),
+
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            parameters=[
+                {'use_sim_time': LaunchConfiguration('use_sim_time')}
+            ]
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[
+                {
+                    'use_sim_time': LaunchConfiguration('use_sim_time'),
+                    'robot_description': Command(['xacro ', LaunchConfiguration('urdf')])
+                }
+            ]
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_path],
+            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        )
+    ])
